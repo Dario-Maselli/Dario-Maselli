2436eed4759b7a4a0d0736eec0da4063fc37f417
Dario Maselli <dario@sinov8.net>
2024-05-29 16:48:28 +0200

    Test Python

--- generate_graphic.py
+++ generate_graphic.py
@@ -3,7 +3,6 @@ from github import Github
 import datetime
 import os
 
-
 def get_contributions(username, token):
     g = Github(token)
     user = g.get_user(username)
@@ -15,7 +14,6 @@ def get_contributions(username, token):
             contributions.append(event.created_at)
 
     return contributions
-
 
 def plot_contributions(contributions):
     dates = [c.date() for c in contributions]
@@ -30,8 +28,20 @@ def plot_contributions(contributions):
 
     plt.savefig('contributions.png')
 
+def count_languages(username, token):
+    g = Github(token)
+    user = g.get_user(username)
+    repos = user.get_repos()
+    languages = set()
 
-def update_readme():
+    for repo in repos:
+        repo_languages = repo.get_languages()
+        for language in repo_languages.keys():
+            languages.add(language)
+
+    return len(languages), languages
+
+def update_readme(languages_count, languages):
     with open("README.md", "r") as file:
         lines = file.readlines()
 
@@ -42,20 +52,23 @@ def update_readme():
                 in_marker = True
                 file.write(line)
                 file.write("\n![Contributions](contributions.png)\n")
+                file.write("## Work In Progress\n")
+                file.write(f"Total Programming Languages Used: {languages_count}\n")
+                file.write("Languages: " + ", ".join(languages) + "\n")
             elif line.strip() == "<!-- END CONTRIBUTIONS -->":
                 in_marker = False
             elif not in_marker:
                 file.write(line)
-
 
 if __name__ == "__main__":
     username = os.getenv('USERNAME')
     token = os.getenv('PAT_TOKEN')
 
     if not username or not token:
-        raise ValueError(
-            "USERNAME and PAT_TOKEN must be set as environment variables.")
+        raise ValueError("USERNAME and PAT_TOKEN must be set as environment variables.")
 
     contributions = get_contributions(username, token)
     plot_contributions(contributions)
-    update_readme()
+    
+    languages_count, languages = count_languages(username, token)
+    update_readme(languages_count, languages)
